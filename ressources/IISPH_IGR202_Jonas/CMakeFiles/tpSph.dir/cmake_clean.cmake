8c217c50a8afa0525831d7b73311d3382d80d693
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-04-25 09:36:04 +0200

    added back many useless files

---
+++ ressources/IISPH_IGR202_Jonas/CMakeFiles/tpSph.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tpSph.dir/dep/glad/src/glad.c.o"
+  "CMakeFiles/tpSph.dir/dep/glad/src/glad.c.o.d"
+  "CMakeFiles/tpSph.dir/src/main.cpp.o"
+  "CMakeFiles/tpSph.dir/src/main.cpp.o.d"
+  "tpSph"
+  "tpSph.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/tpSph.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
