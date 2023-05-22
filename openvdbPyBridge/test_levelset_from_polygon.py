a0773d79dedb9ccb4f2e07c41cc0eebeb2f7be80
adrienctl <adrien.coutelle@telecom-paris.fr>
2023-05-22 16:24:04 +0200

    create numpy grid with voxels

--- openvdbPyBridge/test_levelset_from_polygon.py
+++ openvdbPyBridge/test_levelset_from_polygon.py
@@ -4,23 +4,60 @@ import numpy as np
 
 # this file code uses code from https://www.openvdb.org/documentation/doxygen/python.html to mesh a point cloud
 
-# we must make a triangular mesh out of the points
-
 points = np.loadtxt('points_float.txt')
-print(points)
-print(points.shape)
 grid = vdb.FloatGrid()
 
+# Define dimension of the volume
+max_x = np.max(points[:, 0])
+max_y = np.max(points[:, 1])
+max_z = np.max(points[:, 2])
 
-#grid = grid.createLevelSetFromPolygons(points)
-points = np.random.rand(200, 200, 200)
-print(points.shape)
-print(points)
-grid.copyFromArray(points)
-"""At this point we have a problem of dimensions, 
-we have to figure out what shape the array has to have"""
+min_x = np.min(points[:, 0])
+min_y = np.min(points[:, 1])
+min_z = np.min(points[:, 2])
 
-print(grid.evalActiveVoxelBoundingBox())
+resolution = 500 #arbitrary value
+voxel_size = min(max_x-min_x,max_y-min_y,max_z-min_z)/resolution
+print(voxel_size)
+
+# create empty grid
+
+resolution_x = int(np.ceil((max_x-min_x)/voxel_size))
+resolution_y = int(np.ceil((max_y-min_y)/voxel_size))
+resolution_z = int(np.ceil((max_z-min_z)/voxel_size))
+point_grid = np.zeros((resolution_x, resolution_y, resolution_z))
+
+# fill the grid with the number of particles in each voxel
+
+particle_number = len(points[:,0])
+print("there is ", particle_number, "particles")
+
+particle_count = 0
+for x in np.arange(min_x, max_x, resolution_x):
+    for y in np.arange(min_y, max_y, resolution_y):
+        for z in np.arange(min_z, max_z, resolution_z):
+            for point in points:
+                if (x <= point[0] < x + resolution_x and y <= point[1] < y + resolution_y and z <= point[2] < z + resolution_z):
+                    x_ind, y_ind, z_ind = int(min_x/resolution_x), int(min_y/resolution_y), int(min_z/resolution_z)
+                    point_grid[x_ind][y_ind][z_ind] += 1
+                    particle_count += 1
+
+if (particle_count==particle_number):
+    print("All particles have been added")
+else:
+    print("There are missing particles...")
+
+print(point_grid)
+print(np.sum(point_grid))
+
+
+
+ 
+
+
+
+grid.copyFromArray(point_grid)
+
 grid.name = 'grid'
 
 # those are the same
