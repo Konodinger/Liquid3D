5372f945e48a1d3c4347f051ab3956fbcd27fa31
adrienctl <adrien.coutelle@telecom-paris.fr>
2023-05-29 11:10:48 +0200

    creation of vdb file works in python, but no levelset yet

--- openvdbPyBridge/test_levelset_from_polygon.py
+++ openvdbPyBridge/test_levelset_from_polygon.py
@@ -4,7 +4,7 @@ import numpy as np
 
 # this file code uses code from https://www.openvdb.org/documentation/doxygen/python.html to mesh a point cloud
 
-DEBUG = True
+DEBUG = False
 
 # Write to OBJ file
 def writeObjFile(filename, points, triangles=[], quads=[]):
@@ -37,79 +37,76 @@ print("Start\n")
 points = np.loadtxt('points_float.txt')
 if DEBUG : print("points.shape:", points.shape)
 grid = vdb.FloatGrid()
+grid.name = "density"
 
-# Define dimension of the volume
-max_x = np.max(points[:, 0])
-max_y = np.max(points[:, 1])
-max_z = np.max(points[:, 2])
+def from_points_to_voxel(array, resolution=10):
+    """
+    resolution is an arbitrary value
+    """
+    # Define dimension of the volume
+    max_x = np.max(array[:, 0])
+    max_y = np.max(array[:, 1])
+    max_z = np.max(array[:, 2])
 
-min_x = np.min(points[:, 0])
-min_y = np.min(points[:, 1])
-min_z = np.min(points[:, 2])
+    min_x = np.min(array[:, 0])
+    min_y = np.min(array[:, 1])
+    min_z = np.min(array[:, 2])
+
+    voxel_size = min(max_x-min_x,max_y-min_y,max_z-min_z)/resolution
+
+    # create empty grid
+
+    resolution_x = int(np.ceil((max_x-min_x)/voxel_size))
+    resolution_y = int(np.ceil((max_y-min_y)/voxel_size))
+    resolution_z = int(np.ceil((max_z-min_z)/voxel_size))
+    array_grid = np.zeros((resolution_x, resolution_y, resolution_z))
+
+    if DEBUG:
+        print("resolution_x:", resolution_x)
+        print("resolution_y:", resolution_y)
+        print("resolution_z:", resolution_z)
+
+    # fill the grid with the number of particles in each voxel
+
+    particle_number = len(array[:,0])
+    print("There are", particle_number, "particles")
+
+    particle_count = 0
+    for x in np.arange(min_x, max_x + voxel_size, voxel_size):
+        if DEBUG: print("x:", x)
+        for y in np.arange(min_y, max_y + voxel_size, voxel_size):
+            for z in np.arange(min_z, max_z + voxel_size, voxel_size):
+                for point in array:
+                    if ((x <= point[0] < x + voxel_size) and (y <= point[1] < y + voxel_size) and (z <= point[2] < z + voxel_size)):
+                        x_ind, y_ind, z_ind = int(x/voxel_size), int(y/voxel_size), int(z/voxel_size)
+                        array_grid[x_ind][y_ind][z_ind] += 1
+                        particle_count += 1
+    if DEBUG: 
+        if (particle_count==particle_number):
+            print("All particles have been added in point_grid")
+        else:
+            print("There are missing particles in point_grid...")
+    return array_grid
+
+point_grid = from_points_to_voxel(points, resolution=10)
 
 if DEBUG:
-    print("max_x:", max_x)
-    print("max_y:", max_y)
-    print("max_z:", max_z)
-    print("min_x:", min_x)
-    print("min_y:", min_y)
-    print("min_z:", min_z)
-
-resolution = 10 #arbitrary value
-voxel_size = min(max_x-min_x,max_y-min_y,max_z-min_z)/resolution
-
-# create empty grid
-
-resolution_x = int(np.ceil((max_x-min_x)/voxel_size))
-resolution_y = int(np.ceil((max_y-min_y)/voxel_size))
-resolution_z = int(np.ceil((max_z-min_z)/voxel_size))
-point_grid = np.zeros((resolution_x, resolution_y, resolution_z))
-
-if DEBUG:
-    print("resolution_x:", resolution_x)
-    print("resolution_y:", resolution_y)
-    print("resolution_z:", resolution_z)
-
-# fill the grid with the number of particles in each voxel
-
-particle_number = len(points[:,0])
-print("There are", particle_number, "particles")
-
-particle_count = 0
-for x in np.arange(min_x, max_x + voxel_size, voxel_size):
-    if DEBUG: print("x:", x)
-    for y in np.arange(min_y, max_y + voxel_size, voxel_size):
-        for z in np.arange(min_z, max_z + voxel_size, voxel_size):
-            for point in points:
-                if ((x <= point[0] < x + voxel_size) and (y <= point[1] < y + voxel_size) and (z <= point[2] < z + voxel_size)):
-                    x_ind, y_ind, z_ind = int(x/voxel_size), int(y/voxel_size), int(z/voxel_size)
-                    point_grid[x_ind][y_ind][z_ind] += 1
-                    particle_count += 1
-
-    
-
-if (particle_count==particle_number):
-    print("All particles have been added in point_grid")
-else:
-    print("There are missing particles in point_grid...")
-
-for i in range(point_grid.shape[0]):
-    for j in range(point_grid.shape[1]):
-        for k in range(point_grid.shape[2]):
-            if (point_grid[i][j][k] > 0):
-                print("There are", point_grid[i][j][k], "particles in voxel", i, j, k)
+    for i in range(point_grid.shape[0]):
+        for j in range(point_grid.shape[1]):
+            for k in range(point_grid.shape[2]):
+                if (point_grid[i][j][k] > 0):
+                    print("There are", point_grid[i][j][k], "particles in voxel", i, j, k)
 
 # Convert the numpy array into vdb grid
 
 grid.copyFromArray(point_grid)
-print(grid.activeVoxelCount())
+print("There are",grid.activeVoxelCount(), "active voxels in the grid")
 
 
 
 # those are the same
 #points, triangles, quads = grid.convertToPolygons(adaptivity=0.5)
 mesh = grid.convertToPolygons(adaptivity=0.8)
-print(mesh)
 
 # if not results folder exists, create it
 if not os.path.exists('./results'):
