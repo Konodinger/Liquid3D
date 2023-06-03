97e175ba7b74b645985bbd1f03e6adaf4dc6f4c4
Barthélemy <31370477+BarthPaleologue@users.noreply.github.com>
2023-06-03 19:14:26 +0200

    cleaning docker interface

--- openvdbPyBridge/pythonCppInterface.py
+++ openvdbPyBridge/pythonCppInterface.py
@@ -21,31 +21,27 @@ volume_config = {
 }
 
 
-COMMANDS = [
-    "/root/Liquid3D/openvdbCppBridge/build/OpenVdbBridge -f /root/Liquid3D/openvdbCppBridge/liquidPointCloud_002.txt",
-    f"mkdir {CONTAINER_DIRECTORY}/docker_results",
-    #f"cp /root/Liquid3D/openvdbCppBridge/build/OpenVdbBridge {CONTAINER_DIRECTORY}/docker_results/",
-    #f"cp /root/Liquid3D/openvdbCppBridge/liquidPointCloud_002.txt {CONTAINER_DIRECTORY}/docker_results/",
-    #f"{CONTAINER_DIRECTORY}/docker_results/OpenVdbBridge -f {CONTAINER_DIRECTORY}/docker_results/liquidPointCloud_002.txt",
-    f'ls /results',
-    f"cp -r /results {CONTAINER_DIRECTORY}/docker_results/",
-    f"ls /root/Liquid3D/openvdbCppBridge/build/",
-    f"touch {CONTAINER_DIRECTORY}/docker_results/proof.txt",
-    f"echo 'proof' >> {CONTAINER_DIRECTORY}/docker_results/proof.txt"
-]
+def convert_to_vdb():
+    COMMANDS = [
+        "/root/Liquid3D/openvdbCppBridge/build/OpenVdbBridge -f /root/Liquid3D/openvdbCppBridge/liquidPointCloud_002.txt",
+        f'ls /results',
+        f"cp -r /results {CONTAINER_DIRECTORY}/",
+    ]
 
-container = client.containers.run(image=DOCKER_IMAGE_NAME, command='/bin/sh', tty=True, detach=True,
-                                  # mounts current directory to /root/Liquid3D/openvdbCppBridge/build/ in container
-                                  volumes=volume_config)
+    container = client.containers.run(image=DOCKER_IMAGE_NAME, command='/bin/sh', tty=True, detach=True,
+                                    # mounts current directory to /root/Liquid3D/openvdbCppBridge/build/ in container
+                                    volumes=volume_config)
 
-for COMMAND in COMMANDS:
-    result = container.exec_run(wrapCommand(COMMAND), stream=True, privileged=True)
-    for line in result.output:
-        print(line.decode('utf-8'))
+    for COMMAND in COMMANDS:
+        result = container.exec_run(wrapCommand(COMMAND), stream=True, privileged=True)
+        for line in result.output:
+            print(line.decode('utf-8'))
 
-print("Stopping container")
-container.stop()
-print("Container stopped")
-print("Removing container")
-container.remove()
-print("Container removed")
+    print("Stopping container")
+    container.stop()
+    print("Container stopped")
+    print("Removing container")
+    container.remove()
+    print("Container removed")
+
+convert_to_vdb()
